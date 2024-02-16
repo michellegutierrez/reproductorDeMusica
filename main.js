c10d4ba40b4c311994e65cc54d00957409b4411e
michellegutierrez <micgutierrez17@gmail.com>
2024-02-15 20:16:54 -0500

    ReproductorAnadido

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let buscarBoton = document.getElementById("buscarBoton");
+/*let buscarBoton = document.getElementById("buscarBoton");*/
 
-let canciones = ["Cancion 1", "Cancion 2", "Cancion 3", "Las maanitas", "Cancion 4", "Cancion 5", "Cancion 6", "Cancion 7", "Cancion 8", "Cancion 9", "Cancion 10"];
+/*let canciones = ["Cancion 1", "Cancion 2", "Cancion 3", "Las maanitas", "Cancion 4", "Cancion 5", "Cancion 6", "Cancion 7", "Cancion 8", "Cancion 9", "Cancion 10"];*/
 
-
+/* 
 buscarBoton.addEventListener(
     "click",
     function buscar () {
@@ -52,3 +52,265 @@ buscarBoton.addEventListener(
            });
        }
    );
+ */
+
+   
+
+   class Song {
+
+    constructor(id, nombre, artista, genero, duracion, anio, cover, album, urlSong){
+        this.id = id;
+        this.nombre = nombre;
+        this.artista = artista;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.anio = anio;
+        this.cover = cover;
+        this.album = album;
+        this.urlSong= urlSong; 
+        
+   }
+  getSongName(){
+    return `${this.nombre}`;
+  }
+
+  getSongArtist(){
+    return `${this.artista}`;
+  }
+
+  getSongGender(){
+    return `${this.genero}`;
+  }
+
+  getSongDuration(){
+    return `${this.duracion}`;
+  }
+
+  getSongYear(){
+    return `${this.anio}`;
+  }
+
+  getSongCover(){
+    return `${this.cover}`;
+  }
+
+  getSongUrl(){
+    return `${this.urlSong}`;
+}
+
+}
+
+
+class Playlist {
+    constructor(nombre, listaDeCanciones){
+        this.nombre = nombre;
+        this.listaDeCanciones= listaDeCanciones;
+       
+    }
+    
+    playPlayList(){
+       this.listaDeCanciones.forEach(song =>{
+        console.log(`Se esta reproduciendo: ${song.nombre}`);
+       });
+    }
+
+    getPlaylistName(){
+        return this.nombre + this.listaDeCanciones;
+    }
+   
+    addSongToFavorites(song){
+        return this.listaDeCanciones.push(song);
+    }
+
+    addSongToPlaylist(song){
+        return this.listaDeCanciones.push(song);
+    }
+    
+    removeSongFromFavorites(song){
+       this.listaDeCanciones.filter(cancion => cancion !== song);
+    }
+
+    removeSongFromPlaylist(song){
+        this.listaDeCanciones.filter(cancion => cancion !== song);
+     }
+ 
+
+
+}
+
+
+
+
+
+class   Reproductor {
+  catalogoDeCanciones;
+  cancionActual;
+  audio;
+  currentPlaylist;
+  paused;
+
+   constructor(){
+
+    this.catalogoDeCanciones = [
+
+      new Song("1","Adore you","harry","Pop", "3 minutos", "2025","./Song1Cover.png","album1","./1.mp3"),
+      new Song("2","Physical","dua", "genero2", "3 minutos", "2025","./2cover.png","album1","./2.mp3"),
+      new Song("3","cancion3","autor3", "genero3", "3 minutos", "2025","./imagenes/descarga.jpeg","album1",""),
+      new Song("4","cancion4","autor4", "genero4", "3 minutos", "2025","./imagenes/descarga.jpeg","album1",""),
+      new Song("5","cancion5","autor5", "genero5", "3 minutos", "2025","./imagenes/descarga.jpeg","album1","")
+
+    ] 
+
+    this.mostrarCanciones();
+    this.cancionActual = this.catalogoDeCanciones[0];
+   
+  
+    this.audio = new Audio();
+    this.currentPlaylist ="busqueda";
+
+    let buscar = document.getElementById("buscarBoton");
+    buscar.addEventListener("click",() => {
+      this.buscarCancion(document.getElementById("buscador").value);
+    });
+
+    let play = document.getElementById("play");
+    play.addEventListener('click', () =>{
+      
+     this.play();
+     this.mostrarInfoyPortada();
+    
+    });
+
+     let botonPausa = document.getElementById("pause");
+    botonPausa.addEventListener("click", ()=>{
+      console.log("pausa");
+      this.pause();
+    } );
+
+  let botonStop = document.getElementById("stop");
+  botonStop.addEventListener("click", ()=>{
+    console.log("stop");
+    this.stop();
+  });
+
+   let botonMute = document.getElementById("mute");
+   botonMute.addEventListener("click", () =>{
+    
+    if (this.audio.muted) {
+      this.audio.muted = false;
+      
+  } else {
+      this.audio.muted = true;
+      
+  }
+   }); 
+
+
+
+}
+
+
+   mostrarCanciones(){
+    let canciones = document.getElementById("resBusqueda");
+    this.catalogoDeCanciones.forEach(song =>{
+    
+      canciones.innerHTML += `<div class="contenedorCancion"><p id="res_${song.id}" class="remover">${song.nombre}</p><button class="playListas "><i class="fa-solid fa-play"></i></button><button id="favorito"  class="remover agregarFavoritos"><i class="fa-solid fa-heart"></i></button></i><button  class="remover agregarPlaylist"><i class="fa-solid fa-plus"></i></button></div> `
+     
+   });
+
+  }
+
+   buscarCancion = function (inputUser){
+   
+    inputUser = inputUser.trim(inputUser);
+    inputUser = inputUser.toLowerCase(inputUser);
+    console.log(inputUser);
+    let canciones = document.getElementById("resBusqueda");
+    canciones.innerHTML = '';
+    
+    let resNombre = this.catalogoDeCanciones.filter(song => song.nombre.match(inputUser));
+    let resAlbum = this.catalogoDeCanciones.filter(song => song.album.match(inputUser));
+    let resArtista = this.catalogoDeCanciones.filter(song => song.artista.match(inputUser));
+    
+     let filtroDeCanciones = [...resNombre, ...resAlbum, ...resArtista];
+     filtroDeCanciones = [...new Set(filtroDeCanciones)]
+     this.mostrarBusquedaDeCanciones(filtroDeCanciones);
+  }
+
+  mostrarBusquedaDeCanciones(filtroDeCanciones){
+    let canciones = document.getElementById("resBusqueda");
+    filtroDeCanciones.forEach(song => {
+       
+      canciones.innerHTML = `<div class="contenedorCancion"><p id="res_${song.id}" class="remover">${song.nombre}</p><button class="playListas remover"><i id=""class="fa-solid fa-play"></i></button><button id="favorito"  class="remover agregarFavoritos"><i class="fa-solid fa-heart"></i></button></i><button  class="remover agregarPlaylist"><i class="fa-solid fa-plus"></i></button></div> `
+     
+   });
+  }
+
+ CancionPlay(){
+
+  }
+
+
+
+ stop(){
+  
+   
+    this.cancionActual.currentTime = this.audio.currentTime = 0;
+    this.audio.pause();
+ }
+
+ pause() {
+  if (!this.audio.paused) {
+      this.cancionActual.currentTime = this.audio.currentTime;
+      this.audio.pause();
+  } else {
+      this.audio.play();
+  }
+}
+
+play() {
+  if (this.cancionActual) {
+    if(this.cancionActual !==  undefined){
+      this.audio.src = "/canciones/" + this.cancionActual.urlSong;
+    }
+      if (this.cancionActual.currentTime >= 0) {
+        this.audio.currentTime = this.cancionActual.currentTime; 
+    }
+    this.audio.play();
+  } else {
+
+     
+    this.audio.play();
+    this.audio.paused =true;
+  }
+}
+
+   
+   
+   mostrarInfoyPortada = function(){
+     const portada = document.getElementById("portada");
+     const cancionInfoNombre = document.getElementById("nombreCancion")
+     const cancionInfoDuracion = document.getElementById("duracion");
+     const cancionInfoArtista = document.getElementById("artista");
+     const cancionInfoAlbum = document.getElementById("album");
+     const cancionInfoYear = document.getElementById("year");
+     const cancionInfoGenero = document.getElementById( "genero");
+
+     if (this.cancionActual != undefined) {
+      portada.src = "/imagenes/"+ this.cancionActual.cover;
+     cancionInfoNombre.innerHTML = this.cancionActual.nombre;
+     cancionInfoDuracion.innerHTML = this.cancionActual.duracion;
+     cancionInfoArtista.innerHTML = this.cancionActual.artista;
+     cancionInfoAlbum.innerHTML = this.cancionActual.album;
+     cancionInfoYear.innerHTML= this.cancionActual.anio;
+     cancionInfoGenero.innerHTML = this.cancionActual.genero;
+   }
+
+  }
+}
+
+
+let reproductor = new Reproductor();
+
+
+
